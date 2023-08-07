2df46573d919968c0ff94b607e403bb6026b6ee9
Fabio Reis <82837278+white-Reis@users.noreply.github.com>
2023-08-07 17:11:13 -0300

    feat(Start project): configuring dependencies

---
+++ src/main/java/Fabio/Reis/Scholarship/ScholarshipApplication.java
@@ -0,0 +1,13 @@
+package Fabio.Reis.Scholarship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Fabio/Reis/Scholarship/ScholarshipApplicationTests.java
@@ -0,0 +1,13 @@
+package Fabio.Reis.Scholarship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
